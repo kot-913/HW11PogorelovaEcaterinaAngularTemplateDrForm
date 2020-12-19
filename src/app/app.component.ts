628731edc9235f81b7eff041ea84b3ec53ae75e7
Ecaterina Pogorelova <Ecaterina.Pogorelova@endava.com>
2020-12-19 15:00:39 +0200

    templateForm first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Person {
+  constructor(public name: string, public age: string, public email: string) {}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tamplateForm';
+  name: string = '';
+  age: string = '';
+  email: string = '';
+
+  persons: Person[] = [];
+
+  addItem() {
+    this.persons.push(new Person(this.name, this.age, this.email));
+    this.name = '';
+    this.age = '';
+    this.email = '';
+  }
 }
